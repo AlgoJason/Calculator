e2263136b51b48c86c55c67535f0c5f28bb12177
Jason Dinh <36142741+JDinh6@users.noreply.github.com>
2019-07-14 02:22:11 -0500

    Simple Loop Calculator

--- JavaApplication8.java
+++ JavaApplication8.java
@@ -13,18 +13,18 @@ import java.util.*;
  */
 public class JavaApplication8 {
 
-    public static void calculate(String symbol, int num1, int num2) {
+    public static void calculate(String symbol, float num1, float num2) {
         if ("+".equals(symbol)) {
-            int answer = num1 + num2;
+            float answer = num1 + num2;
             System.out.println(num1 + symbol + "" + num2 + "" + " = " + answer);
         } else if ("-".equals(symbol)) {
-            int answer = num1 - num2;
+            float answer = num1 - num2;
             System.out.println(num1 + symbol + "" + num2 + "" + " = " + answer);
         } else if ("*".equals(symbol)) {
-            int answer = num1 * num2;
+            float answer = num1 * num2;
             System.out.println(num1 + symbol + "" + num2 + "" + " = " + answer);
         } else if ("/".equals(symbol)) {
-            int answer = num1 / num2;
+            float answer = num1 / num2;
             System.out.println(num1 + symbol + "" + num2 + "" + " = " + answer);
         } else {
             System.out.println("error");
@@ -33,27 +33,36 @@ public class JavaApplication8 {
 
     public static void main(String[] args) {
         // TODO code application logic here 
+        boolean i = true;
+        while (i != false) {
+            System.out.println("Please enter a number ");
+            Scanner scanner = new Scanner(System.in);
+            float num1 = scanner.nextFloat();
 
-        System.out.println("Please enter a number ");
-        Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
+            System.out.println("Please enter a second number ");
 
-        System.out.println("Please enter a second number ");
+            float num2 = scanner.nextFloat();
 
-        int num2 = scanner.nextInt();
+            System.out.println("Your numbers are " + num1 + " and " + num2);
 
-        System.out.println("Your numbers are " + num1 + " and " + num2);
+            System.out.println("Please enter the math symbol you would like to use.");
 
-        System.out.println("Please enter the math symbol you would like to use.");
+            scanner.nextLine();
 
-        scanner.nextLine();
+            String symbol = scanner.nextLine();
 
-        String symbol = scanner.nextLine();
+            System.out.println("The symbol you have chosen is " + symbol);
 
-        System.out.println("The symbol you have chosen is " + symbol);
+            calculate(symbol, num1, num2);
+            System.out.println("Do you want to calculate again?");
 
-        calculate(symbol, num1, num2);
-
+            String loop = scanner.nextLine();
+            if ("yes".equals(loop)) {
+                i = true;
+            } else if ("no".equals(loop)) {
+                i = false;
+            }//end if
+        }//end while
     }//end main
 
 }//end class
